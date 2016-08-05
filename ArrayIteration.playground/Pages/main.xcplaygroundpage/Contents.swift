f19863700611c85e8ad616c8b4b9f7ddabeb218b
bhtran <bhtran00@gmail.com>
2016-08-05 18:26:47 -0400

    Completed lab with funcs of ints and strings parameters

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,29 +13,23 @@
 /*: question1
  ### 1. You're building an app and want to store all of the ingredients added to a cart. How would you represent this cart in code? The ingredients are "Chips", "Salsa", "Guacamole", "Red wine". Explicitly mark the content of the cart as strings.
  */
-// write your code here
 
-
-
-
-
+var ingredientsList : [String] = ["Chips", "Salsa", "Gaucamole", "Red wine"]
 
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code? Explicitly mark the content of the cart as numbers.
  */
-// write your code here
-
-
-
-
-
+var listOfNumbers : [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible.
  */
-// write your code here
-
+for number in listOfNumbers {
+    
+    print("\(number)")
+    
+}
 
 
 
@@ -45,8 +39,10 @@
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and print their values in the most efficient way possible. Prefix each item in the array with a string of text.
  */
-// write your code here
-
+for item in ingredientsList {
+    
+    print("\(item)")
+}
 
 
 
@@ -56,9 +52,16 @@
 /*: question5
  ### 5. Take the list of shopping cart items you created in question 1 and print their values by passing each item in list to a function to be printed.
  */
-// write your code here
 
+var newList : [String] = []
 
+for itemThing in ingredientsList {
+    
+    newList.append(itemThing)
+
+}
+
+print(newList)
 
 
 
@@ -69,16 +72,49 @@
  */
 // write your code here
 
+func greetingMachine(names: [String]) -> String {
+    
+    var greeting:String = ""
+    
+    for name in names {
 
+        switch name {
+        case "Michael" :
+            greeting = "Top of the morning \(name)"
+        default:
+            greeting = "Good Morning \(name)"
 
+        }
+        
+    }
+    return greeting
+}
 
+let arrayOfNames : [String] = ["Bao", "Michael", "Adam", "Brent"]
+
+greetingMachine(arrayOfNames)
 
 
 /*: question7
  ### 7. Create a function that takes an array of Ints (40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49) and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
+let arrayOfInts : [Int] = [40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49]
+var newArray : [Int] = []
 
+func returnNewArrayOfInts(arrayOfNumbers: [Int]) -> [Int] {
+    
+    var arrayOfIntsThing : [Int] = []
+    
+    for number in arrayOfNumbers {
+        
+        arrayOfIntsThing.append(number)
+    
+    }
+    
+    return arrayOfIntsThing
+}
+
+newArray = returnNewArrayOfInts(arrayOfInts)
 
 
 
